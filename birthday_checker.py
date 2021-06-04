f7c5caadf3b1fc6aa64ada85ca05ae86612336fa
tjohnson1988 <85202888+tjohnson1988@users.noreply.github.com>
2021-06-03 20:23:06 -0400

    Update birthday_checker.py

--- birthday_checker.py
+++ birthday_checker.py
@@ -1,13 +1,26 @@
 #Take a users input and return their name + birthday. If today is there
 # birthday output “Happy Birthday!”
 
-#test interpreter (this was giving me a major headache)
+# use .strftime to formats dates in a specific manner
+# https://www.programiz.com/python-programming/datetime
+# https://www.youtube.com/watch?v=hj6Tgc4hEU0&ab_channel=CleverProgrammer
+
+from datetime import datetime
+
+current_day = datetime.now().day
+#print(current_day)
+current_month = datetime.now().month
+#print(current_month)
+
 name = input("Enter your name:")
-print(name)
-birth_month = input("Enter your birth month (01-12):")
-print(birth_month)
-birth_day = input("Enter your day of birth:")
-print(birth_day)
+#print(name)
+birth_month = int(input("Enter your birth month (01-12):"))
+#print(birth_month)
+birth_day = int(input("Enter your day of birth:"))
+#print(birth_day)
 birth_year = input("Enter your birth year:")
 
-print("Hi", name + ". Your birthday is", birth_month + "-" + birth_day + "-" + birth_year + ".")
+if (current_day == birth_day) and (current_month == birth_month):
+    print("Happy Birthday!")
+else:
+    print("Hi", name + ". Your birthday is", str(birth_month) + "-" + str(birth_day) + "-" + birth_year + ".")
